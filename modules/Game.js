62902542cfd8ae9de4fda6584389df7a844e9187
George Endrulat <george.endrulat@gmail.com>
2018-08-23 14:07:33 -0500

    moved from server.js

--- modules/Game.js
+++ modules/Game.js
@@ -1,7 +1,40 @@
-
 export const FIELD_WIDTH = 1000;
 export const FIELD_HEIGHT = 1000;
 export let players = {};
 export let bullets = {};
 export let walls = {};
+export function updatePlayers() {
+  Object.values(players).forEach(player => {
+    const movement = player.movement;
+    if (movement.forward) {
+      player.move(player.speed);
+    }
+    if (movement.back) {
+      player.move(-player.speed);
+    }
+    if (movement.left) {
+      player.angle -= player.rotationSpeed;
+    }
+    if (movement.right) {
+      player.angle += player.rotationSpeed;
+    }
+  });
+}
+export function updateBullets() {
+  Object.values(bullets).forEach(bullet => {
+    if (!bullet.move(bullet.speed)) {
+      bullet.remove();
+      return;
+    }
+    Object.values(players).forEach(player => {
+      if (bullet.intersect(player)) {
+        if (player !== bullet.player) {
+          player.damage();
+          bullet.remove();
+          bullet.player.point += 1;
+        }
+      }
+    });
+  });
+}
 
